bcae2a286c58a5f14d68c26ff095892a9315051e
bertojr <1020bertone@gmail.com>
2024-06-10 16:01:05 +0200

    chiamata get nel services pg

--- DnD-Builder/src/app/services/pg.service.ts
+++ DnD-Builder/src/app/services/pg.service.ts
@@ -1,8 +1,16 @@
+import { Observable } from 'rxjs';
+import { iPg } from './../models/i-pg';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PgService {
-  constructor() {}
+  apiUrl = 'http://localhost:3000/pg';
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<iPg[]> {
+    return this.http.get<iPg[]>(this.apiUrl);
+  }
 }
