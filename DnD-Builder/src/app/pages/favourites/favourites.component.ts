f31d18b70d7926e85cd03cf5b5570ca52c143ea8
AlessandroBianco00 <alessandrobianco2000@gmail.com>
2024-06-10 17:20:03 +0200

    Creazione html favorite e versione commentata del ts

--- DnD-Builder/src/app/pages/favourites/favourites.component.ts
+++ DnD-Builder/src/app/pages/favourites/favourites.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { iFavourites } from '../../models/i-favourites';
+import { iUser } from '../../models/i-user';
+import { iPg } from '../../models/i-pg';
 
 @Component({
   selector: 'app-favourites',
@@ -7,4 +10,29 @@ import { Component } from '@angular/core';
 })
 export class FavouritesComponent {
 
+  favouritesArray:iFavourites[] = []
+  arrayPgs:iPg[] = []
+  currentUser!:iUser
+
+  pg!:iPg
+
+  constructor(
+    //Services
+  ) {}
+
+  /*ngOnInit() {
+    const accessData = this.AuthSvc.getAccessData()
+    if(!accessData) return
+    this.currentUser = accessData.user
+    const userId = accessData.user.id
+
+    this.PgSvc.getFavouritePgs(userId).subscribe(favourites => {
+      this.favouritesArray = favourites
+
+      this.favouritesArray.forEach(favorite => {
+        this.PgSvc.getPgById(favorite.idPersonaggio).subscribe(pg => this.arrayPgs.push(pg))
+      })
+    })
+
+  }*/
 }
