e6fb4e864a5d45f2d3b941cf6a1ea4c5515a7cf9
cosmin97 <cosminispir9@gmail.com>
2024-06-11 14:56:06 +0200

    11.06 login

--- DnD-Builder/src/app/auth/login/login.component.ts
+++ DnD-Builder/src/app/auth/login/login.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService} from '../auth.service';
+import { iAuthData } from '../../models/i-auth-data';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
+
 export class LoginComponent {
+  loginData: iAuthData = {
+    email: '',
+    password: ''
+  };
+
+  constructor(
+    private authSvc: AuthService,
+    private router: Router
+  ){}
+
+  signIn(){
+    this.authSvc.login(this.loginData)
+      .subscribe(data => {
+        this.router.navigate(['/dashboard']);
+      });
+    console.log(this.loginData);
+  }
+
 
 }
