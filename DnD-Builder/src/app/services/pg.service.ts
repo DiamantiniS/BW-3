de29c4193a2fcfd0fa81f8874f711d14745551a2
Morgan Grande <morgan.grande@gmail.com>
2024-06-12 15:40:02 +0200

    builder works

--- DnD-Builder/src/app/services/pg.service.ts
+++ DnD-Builder/src/app/services/pg.service.ts
@@ -3,6 +3,9 @@ import { iPg } from './../models/i-pg';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { iClasse } from '../models/i-classe';
+import { iUser } from '../models/i-user';
+import { AuthService } from '../auth/auth.service';
+
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +13,8 @@ import { iClasse } from '../models/i-classe';
 export class PgService {
   apiUrl = 'http://localhost:3000/pg';
   classUrl = 'http://localhost:3000/classe';
-  constructor(private http: HttpClient) {}
+
+  constructor(private http: HttpClient, private AuthSvc: AuthService) {}
 
   getAll(): Observable<iPg[]> {
     return this.http.get<iPg[]>(this.apiUrl);
@@ -29,5 +33,12 @@ export class PgService {
   }
   getClassbyId(classId: number): Observable<iClasse> {
     return this.http.get<iClasse>(`${this.classUrl}/${classId}`);
+  }
+
+  getUserId(): number {
+    const accessData = this.AuthSvc.getAccessData()
+    if(!accessData) return 0
+    const userId = accessData.user.id
+    return userId
   }
 }
