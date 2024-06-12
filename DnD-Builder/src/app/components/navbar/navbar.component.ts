d50646c4a27c8c7ddfd34b1d4c7047f47c119c2f
Simone Diamantini <diama33@gmail.com>
2024-06-12 11:25:23 +0200

    12-06

--- DnD-Builder/src/app/components/navbar/navbar.component.ts
+++ DnD-Builder/src/app/components/navbar/navbar.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
 
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  constructor(private AuthSvc: AuthService) {}
 
-  constructor(private AuthSvc:AuthService) {}
-
-  isLoggedIn = this.AuthSvc.loggedIn
+  isLoggedIn = this.AuthSvc.loggedIn;
 
   ngOnInit() {
-    this.AuthSvc.isLogged$
-    .subscribe(isLoggedIn => this.isLoggedIn = isLoggedIn )
+    this.AuthSvc.isLogged$.subscribe(
+      (isLoggedIn) => (this.isLoggedIn = isLoggedIn)
+    );
+  }
+  logout() {
+    this.AuthSvc.logout();
   }
 }
