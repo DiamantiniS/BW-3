fba61087464bbc911b3b14bba301745e75135ce0
bertojr <1020bertone@gmail.com>
2024-06-12 15:59:50 +0200

    .

--- DnD-Builder/src/app/auth/login/login.component.ts
+++ DnD-Builder/src/app/auth/login/login.component.ts
@@ -6,29 +6,25 @@ import { iAuthData } from '../../models/i-auth-data';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
   loginData: iAuthData = {
-    email: '',
-    password: ''
+    email: 'paulo@paulo.com',
+    password: 'password',
   };
 
-  constructor(
-    private authSvc: AuthService,
-    private router: Router
-  ){}
+  constructor(private authSvc: AuthService, private router: Router) {}
 
   signIn() {
-    this.authSvc.login(this.loginData)
-      .subscribe({
-        next: data => {
-          this.router.navigate(['/dashboard']);
-        },
-        error: err => {
-          console.error('Login error', err);
-        }
-      });
+    this.authSvc.login(this.loginData).subscribe({
+      next: (data) => {
+        this.router.navigate(['/dashboard']);
+      },
+      error: (err) => {
+        console.error('Login error', err);
+      },
+    });
     console.log(this.loginData);
   }
 }
