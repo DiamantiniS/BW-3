670b7d3aaa67f97fc1ecb618a0a3d3ae00145a72
NicoloScivittaro <nscivittaro@gmail.com>
2024-06-13 10:57:04 +0200

    match 0.1

--- DnD-Builder/src/app/pages/showdown/match/match.component.ts
+++ DnD-Builder/src/app/pages/showdown/match/match.component.ts
@@ -1,10 +1,9 @@
+import { iMossa } from './../../../models/i-mossa';
+import { ActivatedRoute } from '@angular/router';
 import { PgService } from './../../../services/pg.service';
 import { Component } from '@angular/core';
 import { iPg } from '../../../models/i-pg';
-import { iUser } from '../../../models/i-user';
-import { UserService } from '../../../services/user.service';
-
-
+import { iClasse } from '../../../models/i-classe';
 @Component({
   selector: 'app-match',
   templateUrl: './match.component.html',
@@ -12,4 +11,35 @@ import { UserService } from '../../../services/user.service';
 })
 export class MatchComponent {
 
+  pg!:iPg
+  classe!:iClasse
+  mosse!:iMossa[]
+
+  constructor(
+    private pgSvc:PgService,
+    private router: ActivatedRoute)
+  {}
+  ngOnInit(): void {
+    this.router.params.subscribe((params) => {
+      this.pgSvc.getById(params['id']).subscribe((pg) => {
+        this.pg=pg
+        this.pgSvc.getClassbyId(pg.classeId).subscribe(
+          classe => {
+            this.classe=classe
+            this.pgSvc.getMosseByIds(classe.mosseId).subscribe(
+              mossa => {
+                this.mosse=mossa
+                console.log(this.mosse)
+                console.log(this.classe)
+                console.log(this.pg)
+              }
+            )
+          }
+        )
+
+
+
+  })
+})
+  }
 }
