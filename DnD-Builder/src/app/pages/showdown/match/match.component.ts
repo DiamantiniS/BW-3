b82cce0a7bfd251ec0e3f7fa1b54be64672eb61c
NicoloScivittaro <nscivittaro@gmail.com>
2024-06-13 12:54:27 +0200

    match 0.2

--- DnD-Builder/src/app/pages/showdown/match/match.component.ts
+++ DnD-Builder/src/app/pages/showdown/match/match.component.ts
@@ -1,45 +1,69 @@
-import { iMossa } from './../../../models/i-mossa';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { PgService } from './../../../services/pg.service';
-import { Component } from '@angular/core';
+import { PgService } from '../../../services/pg.service';
 import { iPg } from '../../../models/i-pg';
 import { iClasse } from '../../../models/i-classe';
+import { iMossa } from '../../../models/i-mossa';
+
 @Component({
   selector: 'app-match',
   templateUrl: './match.component.html',
-  styleUrl: './match.component.scss'
+  styleUrls: ['./match.component.scss']
 })
-export class MatchComponent {
+export class MatchComponent implements OnInit {
 
-  pg!:iPg
-  classe!:iClasse
-  mosse!:iMossa[]
+  pg!: iPg;
+  classe!: iClasse;
+  mosse: iMossa[] = [];
+  botPg!: iPg
+  botMosse: iMossa[] = [];
+  botClasse!:iClasse
+  pgArr: iPg[] = [];
 
   constructor(
-    private pgSvc:PgService,
-    private router: ActivatedRoute)
-  {}
-  ngOnInit(): void {
-    this.router.params.subscribe((params) => {
-      this.pgSvc.getById(params['id']).subscribe((pg) => {
-        this.pg=pg
-        this.pgSvc.getClassbyId(pg.classeId).subscribe(
-          classe => {
-            this.classe=classe
-            this.pgSvc.getMosseByIds(classe.mosseId).subscribe(
-              mossa => {
-                this.mosse=mossa
-                console.log(this.mosse)
-                console.log(this.classe)
-                console.log(this.pg)
-              }
-            )
-          }
-        )
+    private pgSvc: PgService,
+    private route: ActivatedRoute
+    ) {}
 
+    ngOnInit(): void {
+      this.pgSvc.getAll().subscribe((pg) => {
+        this.pgArr = pg;
+        this.getRandomPg()
+    })
 
-
-  })
-})
+    this.route.params.subscribe((params) => {
+      const pgId = params['id'];
+      if (pgId) {
+        this.pgSvc.getById(pgId).subscribe((pg) => {
+          this.pg = pg;
+          this.pgSvc.getClassbyId(pg.classeId).subscribe((classe) => {
+            this.classe = classe;
+            this.pgSvc.getMosseByIds(classe.mosseId).subscribe((mosse) => {
+              this.mosse = mosse;
+              this.pgSvc.getMosseById(21).subscribe((mosse) => {
+                this.mosse.push(mosse);
+                });
+                });
+                });
+                });
+      }
+    });
+  }
+  getRandomPg(){
+    const randomIdPg=Math.floor(Math.random()*this.pgArr.length+1)
+    if (randomIdPg) {
+      this.pgSvc.getById(randomIdPg).subscribe((pg) => {
+        this.botPg = pg;
+        this.pgSvc.getClassbyId(pg.classeId).subscribe((classe) => {
+          this.botClasse = classe;
+          this.pgSvc.getMosseByIds(classe.mosseId).subscribe((mosse) => {
+            this.botMosse = mosse;
+            this.pgSvc.getMosseById(21).subscribe((mosse) => {
+              this.botMosse.push(mosse);
+            });
+          });
+        });
+      });
+    }
   }
 }
--- DnD-Builder/src/app/pages/showdown/showdown.component.ts
+++ DnD-Builder/src/app/pages/showdown/showdown.component.ts
@@ -1,82 +1,105 @@
-import { Component } from '@angular/core';
-import { iPg } from '../../models/i-pg';
-import { PgService } from '../../services/pg.service';
-import { iUser } from '../../models/i-user';
-import { UserService } from '../../services/user.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthService } from '../../auth/auth.service';
+
+import { iPg } from '../../models/i-pg';
+import { iUser } from '../../models/i-user';
 import { iFavourites } from '../../models/i-favourites';
-import { FavouritesService } from '../../services/favourites.service';
 import { iClasse } from '../../models/i-classe';
+
+import { PgService } from '../../services/pg.service';
+import { UserService } from '../../services/user.service';
+import { AuthService } from '../../auth/auth.service';
+import { FavouritesService } from '../../services/favourites.service';
 
 @Component({
   selector: 'app-showdown',
   templateUrl: './showdown.component.html',
-  styleUrl: './showdown.component.scss'
+  styleUrls: ['./showdown.component.scss']
 })
-export class ShowdownComponent {
+export class ShowdownComponent implements OnInit {
   profileForm!: FormGroup;
   user: iUser | null = null;
   characters: iPg[] = [];
 
+  pg!: iPg;
+  favouritesArray: iFavourites[] = [];
+  arrayPgs: iPg[] = [];
+  classPgArray: iClasse[] = [];
 
-
-
-  pg!:iPg
-  favouritesArray:iFavourites[] = []
-  arrayPgs:iPg[] = []
-  classPgArray:iClasse[] = []
   constructor(
     private fb: FormBuilder,
     private userService: UserService,
     private authService: AuthService,
     private router: Router,
-    private PgSvc: PgService,
-    private FavortiteSvc:FavouritesService
-  ){}
+    private pgService: PgService,
+    private favouritesService: FavouritesService
+  ) {}
+
   ngOnInit(): void {
+    this.profileForm = this.fb.group({
+      id: [{ value: '', disabled: true }],
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+
     const accessData = this.authService.getAccessData();
-    console.log('Access Data:', accessData); // Debugging line
     if (accessData) {
       const userId = accessData.user.id;
-      console.log('User ID:', userId); // Debugging line
+
       this.userService.getUserProfile(userId).subscribe({
         next: (user) => {
-          console.log('Fetched User:', user); // Debugging line
           this.user = user;
-          this.profileForm.patchValue(user);
         },
         error: (err) => {
-          console.error('Error fetching user profile', err);
+          console.error('Errore durante il recupero del profilo utente', err);
         },
       });
 
       this.userService.getUserCharacters(userId).subscribe({
         next: (characters) => {
-          console.log('Fetched Characters:', characters); // Debugging line
           this.characters = characters;
         },
         error: (err) => {
-          console.error('Error fetching user characters', err);
+          console.error('Errore durante il recupero dei personaggi utente', err);
         },
       });
 
-    this.PgSvc.getClasses().subscribe(classes => {
-      this.classPgArray = classes
-      console.log(this.classPgArray)
-    })
+      this.pgService.getClasses().subscribe({
+        next: (classes) => {
+          this.classPgArray = classes;
+        },
+        error: (err) => {
+          console.error('Errore durante il recupero delle classi', err);
+        },
+      });
 
-    this.FavortiteSvc.getFavouritePgs(userId).subscribe(favourites => {
-      this.favouritesArray = favourites
-
-      this.favouritesArray.forEach(favorite => {
-        this.PgSvc.getById(favorite.idPersonaggio).subscribe(pg => {
-          this.FavortiteSvc.addClassToPg(pg, this.classPgArray)
-          this.arrayPgs.push(pg)
-        })
-      })
-    })
+      this.favouritesService.getFavouritePgs(userId).subscribe({
+        next: (favourites) => {
+          this.favouritesArray = favourites;
+          this.caricaPersonaggiPreferiti();
+        },
+        error: (err) => {
+          console.error('Errore durante il recupero dei personaggi preferiti', err);
+        },
+      });
+    } else {
+      console.error('Nessun utente attualmente connesso.');
+    }
   }
+
+  caricaPersonaggiPreferiti() {
+    this.favouritesArray.forEach((favorite) => {
+      this.pgService.getById(favorite.idPersonaggio).subscribe({
+        next: (pg) => {
+          this.favouritesService.addClassToPg(pg, this.classPgArray);
+          this.arrayPgs.push(pg);
+        },
+        error: (err) => {
+          console.error(`Errore durante il recupero del personaggio con ID ${favorite.idPersonaggio}`, err);
+        },
+      });
+    });
   }
 }
